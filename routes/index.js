d0fc6f8a94a58d05f136d5cb127920075360904f
reggiej7 <reggiejones7@hotmail.com>
2015-04-21 18:15:40 -0700

    fixing folder structure

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path')
+/* Default  GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+
+router.get('/', function(req, res, next) {
+ // res.render('index', { title: 'Express' });
+ //change above to send {image: 'uwblock.jp'}
+ 
+  res.sendFile(path.join(__dirname, '../views/index.html'))
+	
+	
+});
+
+
+module.exports = router;
