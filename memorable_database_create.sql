61de9b0fa1b544b48dcf676fbcfc71006cc953f0
Saloni Parikh <saloniparikh@Salonis-MacBook-Pro.local>
2015-05-25 21:43:46 -0700

    added more users to db script

--- memorable_database_create.sql
+++ memorable_database_create.sql
@@ -87,6 +87,17 @@ INSERT INTO admin_user VALUES('admin', 'admin');
 INSERT INTO resident_user VALUES('jappleseed', 'test', 'Johnny Appleseed', 'admin', 2);
 INSERT INTO resident_user VALUES('interest', 'interest', 'interest', 'admin', 2);
 
+-- update resident user
+INSERT INTO resident_user VALUES('jappleseed', 'test', 'Grace Johson', 'admin', 2);
+INSERT INTO resident_user VALUES('amartin', 'test', 'Alfredo Martin', 'admin', 2);
+INSERT INTO resident_user VALUES('eshah', 'test', 'Esther Shah', 'admin', 2);
+INSERT INTO resident_user VALUES('fparker', 'test', 'Frank Parker', 'admin', 2);
+INSERT INTO resident_user VALUES('glewis', 'test', 'Gerard Lewis', 'admin', 2);
+INSERT INTO resident_user VALUES('wanderson', 'test', 'Winston Anderson', 'admin', 2);
+INSERT INTO resident_user VALUES('estone', 'test', 'Eileen Stone', 'admin', 2);
+INSERT INTO resident_user VALUES('hwilson', 'test', 'Heather Wilson', 'admin', 2);
+
+
 INSERT INTO song values('fake_song_id_1', 'jappleseed');
 INSERT INTO song values('fake_song_id_2', 'jappleseed');
 
